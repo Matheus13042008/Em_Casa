0823b02c99b014d9c77224625cc8ffc3793d7367
Matheus13042008 <mathbraga.1304@gmail.com>
2026-03-08 21:57:39 -0300

    2 alteracao

--- imc.py
+++ imc.py
@@ -6,15 +6,14 @@ altura = float(input('Qual sua altura?\n'))
 
 print('Seu IMC é:', peso / altura**2)
 
-resto = idade // 7
+resto = idade % 7
 if resto == 0:
     print('Sua idade é divisivel por 7\n')
 else:
     print('Sua idade não é divisivel por 7\n')
 
-resto2 = ano // 4
+resto2 = ano % 4
 if resto2 == 0:
     print('Você nasceu em ano bissexto')
 else:
     print('Você não nasceu em ano bissexto')
-    
